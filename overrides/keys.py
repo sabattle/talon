b8a96461005dd7d5a66ed572b2d5a6fdb1553c5a
Sebastian Battle <sabattle@eckerd.edu>
2022-01-27 00:29:25 -0500

    feat: scaffold project

---
+++ overrides/keys.py
@@ -0,0 +1,13 @@
+from talon import Context, registry, app
+
+ctx = Context()
+
+def _add_keys():
+    """
+    Put this in a launch listener so it runs after knausj
+    """
+    special_keys = registry.lists["user.special_key"][0].copy()
+    special_keys["del"] = special_keys.pop("delete")
+    ctx.lists["self.special_key"] = special_keys
+
+app.register("launch", _add_keys)
