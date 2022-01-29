53dcb98e5a1b2312b0c59fd84d233994443b6b47
Sebastian Battle <sabattle@eckerd.edu>
2022-01-29 00:53:32 -0500

    feat: added and cleaned up capture groups

--- core/keys.py
+++ core/keys.py
@@ -8,11 +8,11 @@ KEYMAPS_PATH = Path(BASE_DIR, "keymaps")
 mod = Module()
 mod.list("letter_key", desc="All letter keys")
 mod.list("symbol_key", desc="All symbol keys")
+mod.list("special_key", desc="All special keys")
 mod.list("arrow_key", desc="All arrow keys")
-mod.list("number_key", desc="All number keys")
 mod.list("modifier_key", desc="All modifier keys")
 mod.list("function_key", desc="All function keys")
-mod.list("special_key", desc="All special keys")
+
 
 ctx = Context()
 
@@ -25,89 +25,64 @@ def _load_lists():
             ctx.lists[f"user.{list_name}"] = safe_load(stream)
 
 
-@mod.capture(rule="{self.modifier_key}+")
-def modifiers(m) -> str:
-    "One or more modifier keys"
-    return "-".join(m.modifier_key_list)
-
-
-@mod.capture(rule="{self.arrow_key}")
-def arrow_key(m) -> str:
-    "One directional arrow key"
-    return m.arrow_key
-
-
-@mod.capture(rule="<self.arrow_key>+")
-def arrow_keys(m) -> str:
-    "One or more arrow keys separated by a space"
-    return str(m)
-
-
-@mod.capture(rule="{self.number_key}")
-def number_key(m) -> str:
-    "One number key"
-    return m.number_key
-
-
+# Letter Key
 @mod.capture(rule="{self.letter_key}")
 def letter_key(m) -> str:
     "One letter key"
     return m.letter_key
 
 
-@mod.capture(rule="{self.special_key}")
-def special_key(m) -> str:
-    "One special key"
-    return m.special_key
+# Number Key
+# Exclusively used with "Unmodified Key" capture
+@mod.capture(rule="{self.number_key}")
+def number_key(m) -> str:
+    "One number key"
+    return m.number_key
 
 
+# Symbol Key
 @mod.capture(rule="{self.symbol_key}")
 def symbol_key(m) -> str:
     "One symbol key"
     return m.symbol_key
 
 
-@mod.capture(rule="{self.function_key}")
-def function_key(m) -> str:
-    "One function key"
-    return m.function_key
+# Special Key
+@mod.capture(rule="{self.special_key}")
+def special_key(m) -> str:
+    "One special key"
+    return m.special_key
 
 
-@mod.capture(rule="( <self.letter_key> | <self.number_key> | <self.symbol_key> )")
-def any_alphanumeric_key(m) -> str:
-    "any alphanumeric key"
-    return str(m)
+# Arrow Key
+@mod.capture(rule="{self.arrow_key}")
+def arrow_key(m) -> str:
+    "One directional arrow key"
+    return m.arrow_key
 
 
+# Modifier Keys
+@mod.capture(rule="{self.modifier_key}+")
+def modifier_keys(m) -> str:
+    "One or more modifier keys"
+    return "-".join(m.modifier_key_list)
+
+
+# Unmodified Key
 @mod.capture(
     rule="( <self.letter_key> | <self.number_key> | <self.symbol_key> "
-    "| <self.arrow_key> | <self.function_key> | <self.special_key> )"
+    "| <self.special_key> | <self.arrow_key> | <self.function_key> )"
 )
 def unmodified_key(m) -> str:
     "A single key with no modifiers"
     return str(m)
 
 
-@mod.capture(rule="{self.modifier_key}* <self.unmodified_key>")
-def key(m) -> str:
-    "A single key with optional modifiers"
-    try:
-        mods = m.modifier_key_list
-    except AttributeError:
-        mods = []
-    return "-".join(mods + [m.unmodified_key])
-
-
-@mod.capture(rule="<self.key>+")
-def keys(m) -> str:
-    "A sequence of one or more keys with optional modifiers"
-    return " ".join(m.key_list)
-
-
-@mod.capture(rule="{self.letter_key}+")
-def letters(m) -> str:
-    "Multiple letter keys"
-    return "".join(m.letter_list)
+# Function Key
+@mod.capture(rule="{self.function_key}")
+def function_key(m) -> str:
+    "One function key"
+    return m.function_key
 
 
 _load_lists()
--- core/keys.talon
+++ core/keys.talon
@@ -1,9 +1,7 @@
-go <user.arrow_keys>: key(arrow_keys)
 <user.letter_key>: key(letter_key)
-(ship | uppercase) <user.letter_keys> [(lowercase | sunk)]:
-    user.insert_formatted(letter_keys, "ALL_CAPS")
 <user.symbol_key>: key(symbol_key)
-<user.function_key>: key(function_key)
 <user.special_key>: key(special_key)
-<user.modifiers> <user.unmodified_key>: key("{modifiers}-{unmodified_key}")
-press <user.modifiers>: key(modifiers)
+go <user.arrow_key>: key(arrow_key)
+<user.modifier_keys> <user.unmodified_key>: key("{modifier_keys}-{unmodified_key}")
+press <user.modifier_keys>: key(modifier_keys)
+<user.function_key>: key(function_key)
